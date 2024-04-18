743b8bba7c9ad0210b5745e786272107336a17d3
Gabe Harvey <harvey7@gmail.com>
2024-04-18 17:17:35 -0500

    Updated Entire File Structure

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import About from './components/About';
-import Projects from './components/Projects';
-import Skills from './components/Skills';
-import Testimonials from './components/Testimonials';
-import Contact from './components/Contact';
+import logo from './logo.svg';
+import './App.css';
 
-export default function App () {
-    return (
-        <main>
-            <Navbar />
-            <About />
-            <Projects />
-            <Skills />
-            <Testimonials />
-            <Contact />
-        </main>
-    )
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
+
+export default App;
