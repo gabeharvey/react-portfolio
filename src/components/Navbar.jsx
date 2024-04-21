2d1db4b504ca1bc9120f477be53854973532dc6d
Gabe Harvey <harvey7@gmail.com>
2024-04-20 23:03:45 -0500

    Added Slide Bar to Navbar.jsx and Minor Styling Updates to index.css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,17 +7,26 @@ const Navbar = () => {
         setNav(!nav)
     }
   return (
-    <div className='flex justify-between items-center'>
-        <h1 className='w-full text-4xl font-bold primary-color ml-2'>Gabe Harvey</h1>
+    <div className='flex justify-between items-center max-w-[1240px] mx-auto h-30 px-4 text-l'>
+        <h1 className='text-3xl font-bold primary-color ml-4'>Gabe Harvey</h1>
         <ul className='hidden md:flex'>
-            <li className='p-3'>About Me</li>
-            <li className='p-3'>Portfolio</li>
-            <li className='p-3'>Contact</li>
-            <li className='p-3'>Resume</li>
+            <li className='p-2'>About Me</li>
+            <li className='p-2'>Portfolio</li>
+            <li className='p-2'>Contact</li>
+            <li className='p-2'>Resume</li>
         </ul>
-        <div onClick={handleNav} className='block md:hidden'>
-            {nav ? <AiOutlineClose size={24}/> : <AiOutlineMenu size={24}/>}
-        </div>
+    <div onClick={handleNav} className='block md:hidden'>
+        {nav ? <AiOutlineClose size={24}/> : <AiOutlineMenu size={24}/>}
+    </div>
+    <div className={nav ? 'text-white-300 fixed h-full left-0 top-0 w-[60%] border-r border-r-gray-900 bg-[#0c0c0c] ease-in-out duration-700' : 'fixed left-[-100%]'}>
+        <h1 className='text-3xl font-bold primary-color ml-4'>Gabe Harvey</h1>
+        <ul className='p-8 text-2xl'>
+            <li className='p-2'>About Me</li>
+            <li className='p-2'>Portfolio</li>
+            <li className='p-2'>Contact</li>
+            <li className='p-2'>Resume</li>
+        </ul>
+    </div>
     </div>
   )
 }
