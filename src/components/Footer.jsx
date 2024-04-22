87256815632d712278dd344ed942570ad58944b8
Gabe Harvey <harvey7@gmail.com>
2024-04-22 11:30:42 -0500

    Added Anchor Tags to Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,10 +6,18 @@ const Footer = () => {
     <div className='mt-2 w-max-[900px] border-t border-gray-300 text-center'>
         <p className='my-5 text-blue-500 text-5xl'>Thank you for Looking!</p>
         <div className='inline-flex text-blue-500 gap-5 text-6xl'>
-            <FaLinkedin/>
-            <FaGithubSquare/>
-            <FaFacebook/>
-            <FaInstagram/>
+            <a href="https://www.linkedin.com/">
+                <FaLinkedin/>
+            </a>
+            <a href="https://github.com/gabeharvey/react-portfolio">
+                <FaGithubSquare/>
+            </a>
+            <a href="https://www.facebook.com/">
+                <FaFacebook/>
+            </a>
+            <a href="https://www.instagram.com/">
+                <FaInstagram/>
+            </a> 
         </div>
     </div>
   )
