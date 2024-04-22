2da7eb8162b36fc7a1f64c5b5d58abac0e443759
Gabe Harvey <harvey7@gmail.com>
2024-04-22 11:21:35 -0500

    Minor Styling Adjustments Throughout

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,23 +1,16 @@
 import React from 'react'
 import proPic from '../assets/profile-pic.png'
 import {TypeAnimation} from 'react-type-animation'
-import {AiFillLinkedin,AiFillGithub,AiFillFacebook,AiFillInstagram} from 'react-icons/ai'
 
 const Hero = () => {
   return (
     <div>
         <div className='max-w-[1200px] h-auto mx-auto flex flex-col-reverse sm:flex-row justify-center align-center'>
         <div className='bg-white-300 flex-col my-auto mx-auto'>
-            <p className='md:text-4xl sm:text-3xl text-xl font-bold text-white-200'>Hello! My name is Gabe Harvey and I am a ...</p>
-            <h1 className='md:text-6xl sm:text-5xl text-3xl font-bold md:py-6'><TypeAnimation sequence={['Full Stack Web Developer',1000,'Designer',1000,'Business Consultant',1000]}wrapper='span' speed={45} repeat={Infinity}/></h1>
+            <p className='md:text-3xl sm:text-3xl text-xl font-bold text-white-200'>Hello! My name is Gabe Harvey and I am a ...</p>
+            <h1 className='md:text-4xl sm:text-5xl text-3xl font-bold md:py-6'><TypeAnimation sequence={['Full Stack Web Developer',1000,'Designer',1000,'Business Consultant',1000]}wrapper='span' speed={45} repeat={Infinity}/></h1>
             <div className='flex justify-center items-center'>
-                <p className='md:text-4xl sm:text-4xl text-xl font-bold text-white-500'>With 2 Bachelor's Degrees from Mississippi State University</p>
-            </div>
-            <div className='text-4xl flex justify-start gap-16 my-6 text-blue-500'>
-                <AiFillLinkedin/>
-                <AiFillGithub/>
-                <AiFillFacebook/>
-                <AiFillInstagram/>
+                <p className='md:text-3xl sm:text-4xl text-xl font-bold text-white-500'>With 2 Bachelor's Degrees from Mississippi State University.</p>
             </div>
         </div>
             <div className='text-7xl'>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,23 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
 
 const Navbar = () => {
-    const [nav, setNav] = useState(false)
+    const [nav, setNav] = useState(false);
+    useEffect(() => {
+        let timer;
+        if (nav) {
+            timer = setTimeout(() => {
+                setNav(false);
+            }, 5000);
+        }
+        return () => clearTimeout(timer);
+    }, [nav]);
     const handleNav = () => {
         setNav(!nav)
     }
   return (
     <div className='flex justify-between items-center max-w-[1240px] mx-auto h-30 px-4 text-l'>
-        <h1 className='text-3xl font-bold primary-color ml-4'>Gabe Harvey</h1>
+        <h1 className='text-5xl font-bold primary-color ml-4'>Gabe Harvey</h1>
         <ul className='hidden md:flex'>
             <li className='p-2'><a href='#about'>About Me</a></li>
             <li className='p-2'><a href='#portfolio'>Portfolio</a></li>
@@ -19,12 +28,12 @@ const Navbar = () => {
         {nav ? <AiOutlineClose size={24}/> : <AiOutlineMenu size={24}/>}
     </div>
     <div className={nav ? 'text-white-300 fixed h-full left-0 top-0 w-[60%] border-r border-r-gray-900 bg-gray-200 ease-in-out duration-700' : 'fixed left-[-100%]'}>
-        <h1 className='text-3xl font-bold primary-color ml-4'>Gabe Harvey</h1>
+        <h1 className='text-5xl font-bold primary-color ml-4'>Gabe Harvey</h1>
         <ul className='p-8 text-2xl'>
-            <li className='p-2'>About Me</li>
-            <li className='p-2'>Portfolio</li>
-            <li className='p-2'>Contact</li>
-            <li className='p-2'>Resume</li>
+            <li className='p-2'><a href='#about'>About Me</a></li>
+            <li className='p-2'><a href='#portfolio'>Portfolio</a></li>
+            <li className='p-2'><a href='#contact'>Contact</a></li>
+            <li className='p-2'><a href='#resume'>Resume</a></li>
         </ul>
     </div>
     </div>
